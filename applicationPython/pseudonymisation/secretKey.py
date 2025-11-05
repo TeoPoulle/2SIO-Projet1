2e2cdb7e0f7fd18998fae296b858e4cc0de63d64
whatZeFox <teo.blns.pll@gmail.com>
2025-11-05 14:24:31 +0100

    update secretKey

--- applicationPython/pseudonymisation/secretKey.py
+++ applicationPython/pseudonymisation/secretKey.py
@@ -1,10 +1,11 @@
-from cryptography.fernet import Fernet
+# Pour les tests, la clé secrète sera celle-ci
+secretKey = b'I2Svw7X7C86fjn_rKcbMF6GG2WJfPH6nP47r-yRFHTU='
 
+"""
+from cryptography.fernet import Fernet
 secretKey = Fernet.generate_key()
 """
-Pour les tests, la clé secrète sera celle-ci : 
-b'I2Svw7X7C86fjn_rKcbMF6GG2WJfPH6nP47r-yRFHTU='
-"""
+
 # chiffrement = Crypto(secretKey)
 # donneesChiffrees = chiffrement.encrypt( <Données à chiffrées> )
 # donneesDechiffrees = chiffrement.decrypt( donneesChiffrees)
